10400192cc807567242d0d3ed2a36b151eca1eaf
PiretLuik <luik.piret@gmail.com>
2025-01-07 21:43:43 +0200

    Fix .gitignore settings and refresh tracked files

---
+++ playwright.config.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const test_1 = require("@playwright/test");
+exports.default = (0, test_1.defineConfig)({
+    testDir: './tests',
+    /* Run tests in files in parallel */
+    fullyParallel: true,
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    forbidOnly: !!process.env.CI,
+    /* Retry on CI only */
+    retries: process.env.CI ? 2 : 0,
+    workers: process.env.CI ? 1 : undefined,
+    reporter: 'html',
+    use: {
+        trace: 'on-first-retry',
+    },
+    projects: [
+        {
+            name: 'API tests',
+        },
+    ],
+});
